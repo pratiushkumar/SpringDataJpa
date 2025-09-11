7b724f4d063a2ea81bee7152053ce29a38b80a30
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-11 23:42:35 +0530

    VAccineDetails

---
+++ VaccineDetails/src/main/java/com/vaccination/VaccineDetails/VaccineDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.vaccination.VaccineDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineDetailsApplication.class, args);
+	}
+
+}
