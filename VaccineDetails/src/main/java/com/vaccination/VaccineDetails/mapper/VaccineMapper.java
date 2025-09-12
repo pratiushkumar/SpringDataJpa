084bc247c59734368c1b6d5e8c81a670072bda62
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-12 23:33:12 +0530

    VaccineMapper

--- VaccineDetails/src/main/java/com/vaccination/VaccineDetails/mapper/VaccineMapper.java
+++ VaccineDetails/src/main/java/com/vaccination/VaccineDetails/mapper/VaccineMapper.java
@@ -10,6 +10,29 @@ public class VaccineMapper
              return null;
          }
          VaccineDTO vt = new VaccineDTO();
-         vt.setId(vt.getId());
+         vt.setId(v.getId());
+         vt.setName(v.getName());
+         vt.setManufacturer(v.getManufacturer());
+         vt.setDosagerequired(v.getDosagerequired());
+         vt.setStorage_temp(v.getStorage_temp());
+         vt.setApproval_date(v.getApproval_date());
+         vt.setExpiry_date(v.getExpiry_date());
+         vt.setDescription(v.getDescription());
+         return vt;
+    }
+    public static Vaccine toEntity(VaccineDTO d) {
+        if (d == null) {
+            return null;
+        }
+        Vaccine dt = new Vaccine();
+        dt.setId(d.getId());
+        dt.setName(d.getName());
+        dt.setManufacturer(d.getManufacturer());
+        dt.setDosagerequired(d.getDosagerequired());
+        dt.setStorage_temp(d.getStorage_temp());
+        dt.setApproval_date(d.getApproval_date());
+        dt.setExpiry_date(d.getExpiry_date());
+        dt.setDescription(d.getDescription());
+        return dt;
     }
 }
