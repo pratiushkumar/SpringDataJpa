9fe3539ea3622550ffd9114030a842b71595be96
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-13 23:56:21 +0530

    ResourceNotFound

---
+++ VaccineDetails/src/main/java/com/vaccination/VaccineDetails/exception/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.vaccination.VaccineDetails.exception;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message ) {
+    super(message);
+    }
+}
